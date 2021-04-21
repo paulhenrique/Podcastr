f2fff821bb9140fc4ff60906f1d8e1865f639325
Paulo Henrique <phvcandido@gmail.com>
2021-04-21 11:29:07 -0300

    Adicionando formação dos episódios

---
+++ src/utils/convertDurationToTimeString.ts
@@ -0,0 +1,9 @@
+export function convertDurationToTimeStyring(duration: number) {
+  const hours = Math.floor(duration / 3600)
+  const minutes = Math.floor(Math.floor(duration % 3600) / 60)
+  const seconds = duration % 60;
+
+  const timeString = [hours, minutes, seconds].map(unit => String(unit).padStart(2, '0')).join(':')
+
+  return timeString;
+}
